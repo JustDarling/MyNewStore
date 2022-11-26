9baa79438748a7ff5cf54f131821d5b6089c1ba3
Kolya <kolyaz.officialx@gmail.com>
2022-11-27 00:30:22 +0200

    .

---
+++ assets/js/main.js
@@ -0,0 +1,14 @@
+// const dropdown = document.querySelector('.dropdown');
+// const content = document.querySelector('.dropdown-content');
+// dropdown.addEventListener('mouseenter', function(e) {
+//     content.style.display='block';
+// });
+// dropdown.addEventListener('mouseout', function(e) {
+//     content.style.display='none';
+// });
+// content.addEventListener('mouseenter', function(e) {
+//     content.style.display='block';
+// });
+// content.addEventListener('mouseout', function(e) {
+//     content.style.display='none';
+// });
